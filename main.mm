3ede587a51a78e4ede02b5ecb43eb9005202c7ec
deVbug <devbug@devbug.me>
2012-04-27 05:26:24 +0900

    fix lossing log lines

--- main.mm
+++ main.mm
@@ -26,11 +26,13 @@
 #include <sys/socket.h>
 
 
+#define BUFFER_SIZE			80
+
 int main(int argc, char **argv, char **envp)
 {
 	int sockfd;
 	int readlen;
-	char buffer[1025];
+	char *buffer;
 
 	struct sockaddr_un server_addr;
 
@@ -50,19 +52,54 @@ int main(int argc, char **argv, char **envp)
 	}
 
 	while (1) {
-		memset(buffer, 0, 1025);
-		readlen = read(sockfd, buffer, 1024);
+		buffer = (char *)malloc(BUFFER_SIZE*sizeof(char));
+		memset(buffer, 0, BUFFER_SIZE);
+		readlen = read(sockfd, buffer, BUFFER_SIZE-1);
 
+		if (readlen >= BUFFER_SIZE-1) {
+			int realloc_count = 1;
+			char *realloc_ptr = NULL;
+			while (readlen >= BUFFER_SIZE-1) {
+				realloc_count++;
+				buffer = (char *)realloc(buffer, BUFFER_SIZE*sizeof(char)*realloc_count);
+				realloc_ptr = buffer+strlen(buffer);
+				readlen = read(sockfd, realloc_ptr, BUFFER_SIZE-1);
+				*(realloc_ptr+readlen) = 0x0;
+				while (realloc_ptr+readlen != buffer+strlen(buffer)) {
+					int i, len = strlen(buffer);
+					for (i=-1;*(buffer+len+i)=='\n';i--)
+						*(buffer+len+i) = ' ';
+					for (i=1;*(buffer+len+i)=='\n';i++)
+						*(buffer+len+i) = ' ';
+					i--;
+					*(buffer+len+i) = '\n';
+					if (i != 0)
+						*(buffer+len) = ' ';
+				}
+
+				if (readlen == 0) break;
+			}
+		}
 		if (readlen == 0) break;
 
 		if (0 == strcmp("\n========================\nASL is here to serve you\n> ", buffer)) {
 			strcpy(buffer, "watch");
 			write(sockfd, buffer, strlen(buffer)+1);
-		}
-		if (0 == strcmp("> ", buffer)) continue;
 
-		printf("%s", buffer);
+			free(buffer);
+			continue;
+		}
+		if (0 == strcmp("> ", buffer)) {
+			free(buffer);
+			continue;
+		}
+
+		printf("%s", (buffer[0] == '\n' ? buffer+1 : buffer));
+
+		free(buffer);
 	}
+
+	free(buffer);
 
 	close(sockfd);
 
